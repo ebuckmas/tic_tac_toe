984b7601ac92e7d81e8fac74d706ffed49431ec1
ebuckmas <evanbuckmaster@gmail.com>
2022-09-30 16:01:45 -0400

    gameboard logic

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hello world')
+
+const gameBoard = (() => {
+    //public
+
+    //private
+    const board = Array.from(' '.repeat(9));
+    
+    const getBox = Array.from(document.getElementsByClassName('box'));
+    
+    const fill = getBox.forEach((box, index) => box.innerText = board[index]);
+    getBox.forEach(box => {        
+        box.addEventListener('mouseover', (e) => {
+            console.log(e.target.id); 
+        });
+    
+        box.addEventListener('click', (e) => {
+            console.log(e.target.id + 'hello')
+        });
+        
+        
+    });
+        
+})();
+
+
+
+
+
+
+
+
